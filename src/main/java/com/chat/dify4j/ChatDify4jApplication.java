cd205a992f571f5ab3a657f7ce7a9527f34f94fb
YYYYYDDDDD <52437468+YYYYYDDDDD@users.noreply.github.com>
2024-08-20 17:49:54 +0800

    移除重复配置

--- src/main/java/com/chat/dify4j/ChatDify4jApplication.java
+++ src/main/java/com/chat/dify4j/ChatDify4jApplication.java
@@ -1,12 +1,9 @@
 package com.chat.dify4j;
 
-import com.chat.dify4j.properties.ChatUrlProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(value = ChatUrlProperties.class)
 public class ChatDify4jApplication {
 
 	public static void main(String[] args) {
